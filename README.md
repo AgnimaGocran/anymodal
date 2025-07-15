9d50304a0374dacb699d3badc0b8b62034c65007
Artemetrix <arhimondus@vk.com>
2025-07-15 11:03:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# AnyModal
+
+This library allows you to centrally manage modal windows with strict typing. You can pass any typed data to a modal window.
+
+## Installation
+
+```bash
+bun add AgnimaGocran/anymodal
+yarn add AgnimaGocran/anymodal
+```
+
+## Setup
+
+Set up the modal manager anywhere in your project (for example, in `@/modals/modals.ts`):
+
+```ts
+import anyModal from 'any-modal';
+
+const modals = anyModal<
+	| { type: 'view-article', articleId: number }
+	| { type: 'new-article', categoryId: number }
+	| { type: 'another-modal', param1: string, param2: number, param3: number[] }
+>();
+
+export default modals;
+```
+
+## Create a modals
+
+Create modal `@/modals/view-article.ts` (use modal component from any library, example with ReactModal):
+
+```ts
+export default modals.create('view-article', ({ modal: { articleId } }) => {
+	return <ReactModal
+		onRequestClose={() => modals.prev()}
+		shouldCloseOnEsc={true}
+		isOpen={true} // Modal renders only when invoked, so no need to manage this manually
+	>
+		Your content of the view-article modal of article #{articleId} 
+	</ReactModal>;
+```
+
+## Modals loader
+
+Create a loader file, such as `@/modals/loader.ts`, to register your modals and configure the modal library (make sure to invoke this component in your layout or app root):
+
+```ts
+'use client';
+
+import ViewArticle from '@/modals/view-article';
+import NewArticle from '@/modals/new-article';
+import AnotherModal from '@/modals/another-modal';
+import { useEffect } from 'react';
+import ReactModal from 'react-modal';
+
+export default function ModalsLoader() {
+	useEffect(() => {
+		ReactModal.setAppElement('#modal-root');
+	}, []);
+
+	return <>
+		<ViewArticle/>
+		<NewArticle/>
+		<AnotherModal/>
+		<div id="modal-root"></div>
+	</>;
+}
+```
+
+## Usage
+
+In any component or page:
+
+```ts
+import modals from '@modals/modals';
+
+export default MyPageComponent() {
+	return <div>
+		<button onClick={() => modals.show({ type: 'view-article', articleId: 15 })}>
+			View article
+		</button>
+		<button onClick={() => modals.show({ type: 'new-article', categoryId: 6 })}>
+			New article
+		</button>
+		<button onClick={() => modals.show({ type: 'another-modal', param1: 'param1', param2: 2, param3: [3, 2, 1] })}>
+			Another modal
+		</button>
+	</div>;
+}
+```
---
+++ src/index.tsx
@@ -0,0 +1,122 @@
+import type { ReactNode } from 'react';
+import { useSnapshot } from 'valtio/react';
+import { useAsync } from '@siberiacancode/reactuse';
+import { proxy } from 'valtio';
+
+export default function anyModal<AnyModals extends { type: string }>(
+	loaderNode: (modal: AnyModals) => ReactNode | null = () => null,
+	errorNode: (error: Error) => ReactNode | null = () => null
+) {
+	interface AnyModalsState {
+		modal: AnyModals | null;
+		modalsStack: AnyModals[];
+	}
+
+	const modalsState = proxy<AnyModalsState>({
+		modal: null,
+		modalsStack: [],
+	});
+
+	type ExtractModal<T extends NonNullable<AnyModals>['type']> = Extract<NonNullable<AnyModals>, { type: T }>;
+
+	interface ModalProps<T extends NonNullable<AnyModals>['type']> {
+		type: T;
+		render: (modal: ExtractModal<T>) => ReactNode;
+	}
+
+	function show(modal: AnyModals) {
+		if (modalsState.modal) {
+			modalsState.modalsStack.push(modalsState.modal);
+		}
+		modalsState.modal = modal;
+	}
+
+	function prev() {
+		modalsState.modal = modalsState.modalsStack.pop() || null;
+	}
+
+	function closeAll() {
+		modalsState.modalsStack = [];
+		modalsState.modal = null;
+	}
+
+	function Modal<T extends NonNullable<AnyModals>['type']>({ type, render }: ModalProps<T>) {
+		const { modal } = useSnapshot(modalsState);
+
+		if (!modal || modal.type !== type) {
+			return null;
+		}
+
+		return render(modal as ExtractModal<T>);
+	}
+
+	function create<T extends NonNullable<AnyModals>['type']>(
+		type: T,
+		Body: React.FC<{ modal: ExtractModal<T> }>
+	) {
+		return function Wrapper() {
+			return <Modal
+			type={type}
+			render={(modal) => <Body modal={modal} />}
+			/>;
+		};
+	}
+
+	type ModalType = NonNullable<AnyModals>['type'];
+	type ModalOf<K extends ModalType> = Extract<AnyModals, { type: K }>;
+
+	type Result<F> =
+	F extends readonly (infer P)[]
+	? { [I in keyof F]: Awaited<F[I]> }
+	: never;
+
+	function createWithFetch<
+	K extends ModalType,
+	F
+	>(
+		kind: K,
+		fetchersFn: (m: ModalOf<K>) => F,
+	  Body: React.FC<{
+		  modal: ModalOf<K>;
+		  data: Result<F>;
+	  }>
+	) {
+		function ModalContent({ modal }: { modal: ModalOf<K> }) {
+			const { isLoading, data, error } = useAsync<Result<F>>(
+				async () => {
+					const promises = fetchersFn(modal) as unknown as readonly unknown[];
+					return await Promise.all(
+						promises as readonly unknown[]
+					) as unknown as Result<F>;
+				},
+				[modal],
+			);
+
+			if (error) {
+				return errorNode(error);
+			}
+
+			if (isLoading || !data) {
+				return loaderNode(modal);
+			}
+
+			return <Body modal={modal} data={data} />;
+		}
+
+		return function Wrapper() {
+			return <Modal
+			type={kind}
+			render={(modal) => <ModalContent modal={modal} />}
+			/>;
+		};
+	}
+
+	return {
+		show,
+		prev,
+		closeAll,
+		create,
+		createWithFetch,
+		modalsState,
+	};
+}
